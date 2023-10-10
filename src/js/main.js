ee16666eee137b473884e6f0078833785d364799
Yuan Zhang <yu_an12@163.com>
2023-10-10 18:22:26 -0400

    bounce + shadow

--- src/js/main.js
+++ src/js/main.js
@@ -5,28 +5,22 @@ import * as bootstrap from 'bootstrap'
 function aboutContent() {
     let aboutContent = document.getElementById("about");
     // aboutContent.innerHTML = "Sunday Afternoon is an award-winning BIPOC & Women-owned creative company. We create impactful culture-expanding work for established and challenger brands that are looking to shake up the status quo. We believe in a fundamentally diverse and progressive approach to problem solving through design. A combination of relentless curiosity, forward-thinking technology and access to a global pool of renowned artists leads to holistic brand work that is uniquely built to be future-proof in an IRL-to-URL World."
-    const a = "Sunday Afternoon is an ";
-    const b = "award-winning ";
-    const c = "BIPOC & Women-owned ";
-    const d = "creative company. We create impactful culture-expanding work for established and challenger brands that are looking to shake up the status quo. We believe in a fundamentally diverse and progressive approach to problem solving through design. A combination of relentless curiosity, forward-thinking technology and access to a global pool of renowned artists leads to holistic brand work that is uniquely built to be future-proof in an IRL-to-URL World.";
-    const span1 = document.createElement("span")
-    const span2 = document.createElement("span")
-    const span3 = document.createElement("span")
-    const span4 = document.createElement("span")
-    span1.innerHTML = a
-    span2.innerHTML = b
-    span3.innerHTML = c
-    span4.innerHTML = d
-    aboutContent.appendChild(span1)
-    aboutContent.appendChild(span2)
-    aboutContent.appendChild(span3)
-    aboutContent.appendChild(span4)
-    colorInteraction(span3)
+    let textarray = ["Sunday Afternoon", " is an ", "award-winning ", "BIPOC & Women-owned", " creative company. We create impactful culture-expanding work for established and challenger brands that are looking to shake up the status quo. We believe in a fundamentally diverse and progressive approach to problem solving through design. A combination of relentless curiosity, forward-thinking technology and access to a global pool of renowned artists leads to holistic brand work that is uniquely built to be future-proof in an ", "IRL-to-URL", " World."]
+    let spanarray = []
+    for (let i = 0; i < textarray.length; i++) {
+        let textcontent = textarray[i]
+        let span = document.createElement("span")
+        span.innerHTML = textcontent
+        aboutContent.appendChild(span)
+        spanarray.push(span)
+    }
+    shadowanime(spanarray[0])
+        // spacing(spanarray[5])
+    bounce(spanarray[3])
 }
 aboutContent()
 
 function colorInteraction(span) {
-
     span.addEventListener('mouseover', function() {
         let colors = [
             '#FF0000',
@@ -46,16 +40,39 @@ function colorInteraction(span) {
         function changeColor() {
             span.style.color = colors[index];
             index++;
-
-            if (index < colors.length) {
-                setTimeout(changeColor, 200); // Change color every 0.5 seconds
+            if (index !== 0 || index < colors.length) {
+                setTimeout(changeColor, 350); // Change color every 0.5 seconds
             }
         }
         changeColor();
     });
-
     span.addEventListener('mouseout', function() {
-        // Reset color back to black when mouse leaves the element
         span.style.color = 'black';
+    });
+}
+
+
+function shadowanime(span) {
+    span.classList.add("shadowanima");
+}
+
+function bounce(span) {
+    span.innerHTML = span.textContent.split('').map(function(char) {
+        if (char === ' ') {
+            return char;
+        }
+        return '<span class="bounce">' + char + '</span>';
+    }).join('');
+    span.addEventListener('mouseenter', function() {
+        var chars = span.querySelectorAll('.bounce');
+        console.log(chars)
+        chars.forEach(function(char, i) {
+            setTimeout(function() {
+                char.classList.add('wave');
+            }, i * 100);
+            setTimeout(function() {
+                char.classList.remove('wave');
+            }, i * 100 + 350);
+        });
     });
 }
