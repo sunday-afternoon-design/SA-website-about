fbc0bebe0be8d2a4ab396cf59c0e2d38fbc6b6e0
Yuan Zhang <yu_an12@163.com>
2023-10-16 12:04:41 -0400

    1016

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,7 @@ import * as bootstrap from 'bootstrap'
 
 function aboutContent() {
     let aboutContent = document.getElementById("about");
-    // aboutContent.innerHTML = "Sunday Afternoon is an award-winning BIPOC & Women-owned creative company. We create impactful culture-expanding work for established and challenger brands that are looking to shake up the status quo. We believe in a fundamentally diverse and progressive approach to problem solving through design. A combination of relentless curiosity, forward-thinking technology and access to a global pool of renowned artists leads to holistic brand work that is uniquely built to be future-proof in an IRL-to-URL World."
-    let textarray = ["Sunday Afternoon", " is an ", "award-winning ", "BIPOC & Women-owned", " creative company. We create impactful culture-expanding work for established and challenger brands that are looking to shake up the status quo. We believe in a fundamentally diverse and progressive approach to problem solving through design. A combination of relentless curiosity, forward-thinking technology and access to a global pool of renowned artists leads to holistic brand work that is uniquely built to be future-proof in an ", "IRL-to-URL", " World."]
+    let textarray = ["Sunday Afternoon", " is an ", "award-winning ", "BIPOC & Women-<br>owned", " creative company. We create impactful culture-expanding work for established and challenger brands that are looking to shake up the status quo. We believe in a fundamentally diverse and progressive approach to problem solving through design. A combination of relentless curiosity, forward-thinking technology and access to a global pool of renowned artists leads to ", "holistic brand work", " that is <br>uniquely built to be future-proof in an ", "IRL-to-URL", " World."]
     let spanarray = []
     for (let i = 0; i < textarray.length; i++) {
         let textcontent = textarray[i]
@@ -15,10 +14,56 @@ function aboutContent() {
         spanarray.push(span)
     }
     shadowanime(spanarray[0])
-        // spacing(spanarray[5])
     bounce(spanarray[3])
+    kerning(spanarray[5])
+    rotate(spanarray[7])
 }
 aboutContent()
+
+
+
+
+function shadowanime(span) {
+    span.classList.add("shadowanima");
+}
+
+function rotate(span) {
+    span.classList.add("rotateCss");
+}
+
+function kerning(span) {
+    span.classList.add("kerning");
+}
+
+function bounce(span) {
+    span.innerHTML = span.textContent.split('').map(function(char) {
+        if (char === ' ') {
+            return char;
+        }
+        return '<span class="bounce">' + char + '</span>';
+    }).join('');
+    span.addEventListener('mouseenter', function() {
+        var chars = span.querySelectorAll('.bounce');
+        console.log(chars)
+        chars.forEach(function(char, i) {
+            setTimeout(function() {
+                char.classList.add('wave');
+            }, i * 100);
+            setTimeout(function() {
+                char.classList.remove('wave');
+            }, i * 100 + 350);
+        });
+    });
+}
+
+
+
+
+
+
+
+
+
 
 function colorInteraction(span) {
     span.addEventListener('mouseover', function() {
@@ -48,31 +93,5 @@ function colorInteraction(span) {
     });
     span.addEventListener('mouseout', function() {
         span.style.color = 'black';
-    });
-}
-
-
-function shadowanime(span) {
-    span.classList.add("shadowanima");
-}
-
-function bounce(span) {
-    span.innerHTML = span.textContent.split('').map(function(char) {
-        if (char === ' ') {
-            return char;
-        }
-        return '<span class="bounce">' + char + '</span>';
-    }).join('');
-    span.addEventListener('mouseenter', function() {
-        var chars = span.querySelectorAll('.bounce');
-        console.log(chars)
-        chars.forEach(function(char, i) {
-            setTimeout(function() {
-                char.classList.add('wave');
-            }, i * 100);
-            setTimeout(function() {
-                char.classList.remove('wave');
-            }, i * 100 + 350);
-        });
     });
 }
